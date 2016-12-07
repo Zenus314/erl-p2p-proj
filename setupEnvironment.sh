0ffe9e3c9449e8c647442a7781ffe84a070b2d04
This man has no name <matteo.badaracco94@gmail.com>
2016-12-07 17:44:27 +0100

    Add files via upload

--- setupEnvironment.sh
+++ setupEnvironment.sh
@@ -6,9 +6,18 @@
       for i in `seq 0 7`;
           do  
               ssh ${1}@diufpc8${i}.unifr.ch "mkdir -p mpe/erl/teda/p2p_shared_files"
-
           done 
 
        #list of file to send
-       scp ../../p2p_shared_files/sam ${1}@diufpc84.unifr.ch:mpe/erl/teda/p2p_shared_files/
+       for i in `seq 0 6`;
+          do  
+              scp ../../p2p_shared_files/southpath.avi ${1}@diufpc84.unifr.ch:mpe/erl/teda/p2p_shared_files/
+              scp ../../p2p_shared_files/sam.mp4 ${1}@diufpc84.unifr.ch:mpe/erl/teda/p2p_shared_files/
+          done
+      
+       for i in `seq 0 3`;
+          do 
+              scp ../../p2p_shared_files/isengard.mp4 ${1}@diufpc84.unifr.ch:mpe/erl/teda/p2p_shared_files/
+              scp ../../p2p_shared_files/games.mp4 ${1}@diufpc84.unifr.ch:mpe/erl/teda/p2p_shared_files/
+          done
 #fi
