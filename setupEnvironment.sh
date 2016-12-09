045e3b62f9f5d0a91585ffe947cdddc4b80c249f
This man has no name <matteo.badaracco94@gmail.com>
2016-12-09 15:37:25 +0100

    Add files via upload

--- setupEnvironment.sh
+++ setupEnvironment.sh
@@ -3,21 +3,24 @@
 #if [-z "$1"]
   #then echo "Put as parameter your diuf username"
   #else 
-      for i in `seq 0 7`;
+      for i in `seq 0 6`;
           do  
               ssh ${1}@diufpc8${i}.unifr.ch "mkdir -p mpe/erl/teda/p2p_shared_files"
           done 
 
        #list of file to send
-       for i in `seq 0 6`;
+       for i in `seq 0 5`;
           do  
-              scp ../../p2p_shared_files/southpath.avi ${1}@diufpc84.unifr.ch:mpe/erl/teda/p2p_shared_files/
-              scp ../../p2p_shared_files/sam.mp4 ${1}@diufpc84.unifr.ch:mpe/erl/teda/p2p_shared_files/
+              #scp ../../p2p_shared_files/rednose.mp4 ${1}@diufpc8${i}.unifr.ch:mpe/erl/teda/p2p_shared_files/
+              #scp ../../p2p_shared_files/sam.mp4 ${1}@diufpc8${i}.unifr.ch:mpe/erl/teda/p2p_shared_files/
+              #scp ../../p2p_shared_files/lorem.txt ${1}@diufpc8${i}.unifr.ch:mpe/erl/teda/p2p_shared_files/
+              scp ../../p2p_shared_files/games.mp4 ${1}@diufpc8${i}.unifr.ch:mpe/erl/teda/p2p_shared_files/
+              scp ../../p2p_shared_files/15min.mp4 ${1}@diufpc8${i}.unifr.ch:mpe/erl/teda/p2p_shared_files/
+              scp ../../p2p_shared_files/30min.mp4 ${1}@diufpc8${i}.unifr.ch:mpe/erl/teda/p2p_shared_files/
           done
       
        for i in `seq 0 3`;
           do 
-              scp ../../p2p_shared_files/isengard.mp4 ${1}@diufpc84.unifr.ch:mpe/erl/teda/p2p_shared_files/
-              scp ../../p2p_shared_files/games.mp4 ${1}@diufpc84.unifr.ch:mpe/erl/teda/p2p_shared_files/
+              scp ../../p2p_shared_files/isengard.mp4 ${1}@diufpc8${i}.unifr.ch:mpe/erl/teda/p2p_shared_files/
           done
 #fi
